7cbafa21e787954651e550d8c21b51c92fc0b2bc
jhk0530 <hwanistic@gmail.com>
2020-02-13 22:36:31 +0900

    Update findPathway.R
    moved to inside cellenrich function

--- R/findPathway.R
+++ R/findPathway.R
@@ -1,13 +1,3 @@
 #' @export
 #'
 #'
-findPathway = function(s){
-  res = list()
-  for(i in 1:length(s)){
-    pvh = getHyperPvalue(s[[i]] , genesets)
-    qvh = p.adjust(pvh, 'fdr')
-    res[[i]] = unname(which(qvh<0.1))
-  }
-
-  return(res)
-}
