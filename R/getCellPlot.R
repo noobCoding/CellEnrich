a14c5e3093548202093452dfaabd68de7af4bed5
jhk0530 <hwanistic@gmail.com>
2020-04-09 22:31:33 +0900

    highchart added

--- R/getCellPlot.R
+++ R/getCellPlot.R
@@ -1,14 +1,27 @@
 getCellPlot = function(dfobj, Cells){
+
+  cat('getCellPlot\n')
+  require(highcharter)
+
   colnames(dfobj) <- c("x", "y", "col")
   dfobj <<- dfobj
+
 
   source('R/briterhex.R')
 
   UniqueCol <- briterhex(scales::hue_pal()(length(Cells)))
   names(UniqueCol) <- Cells
-  colV <- unname(UniqueCol[dfobj$col])
-  return(
-    ggplot(dfobj, aes(x = x, y = y)) +
-      geom_point(colour = colV)
-  )
+
+  dfobj$col <- unname(UniqueCol[dfobj$col])
+
+  hc <- hchart(
+    dfobj,
+    type = 'scatter',
+    hcaes(x = x, y = y, color = col)
+  ) %>% hc_tooltip(FALSE)
+
+  cat('\n')
+
+  return(hc)
+
 }
