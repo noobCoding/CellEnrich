d6336d1f21d7e3f77bb1351213ea6290e2dbe27e
jhk0530 <hwanistic@gmail.com>
2020-04-10 17:11:50 +0900

    path with highcharter

--- R/emphasize.R
+++ R/emphasize.R
@@ -1,6 +1,6 @@
 #' @export
 
-emphasize <- function(path = FALSE, inputObj, dfobj, Cells, pres) {
+emphasize <- function(path = FALSE, inputObj, dfobj, Cells, pres, genesets) {
   cat('emphasize\n')
   buildRlobj <- function(items) {
 
@@ -64,38 +64,58 @@ emphasize <- function(path = FALSE, inputObj, dfobj, Cells, pres) {
   UniqueCol <- briterhex(scales::hue_pal()(length(Cells)))
   names(UniqueCol) <- Cells
 
-  colV <- unname(UniqueCol[dfobj$col])
+  colV <- unname(UniqueCol[dfobj_new$col])
 
   colV[-unlist(cellValues, use.names = FALSE)] <- "#95A5A6" # gray color
 
-  graphString <- "ggobj2 <- ggplot(dfobj_new, aes(x = x, y = y)) + geom_point(colour = colV)"
+  dfobj_new$col <- colV
+  rownames(dfobj_new) = NULL
+
+  hc <- hchart(
+      dfobj_new,
+      type = 'scatter',
+      hcaes(x = x, y = y, color = col)
+    ) %>%
+    hc_tooltip(FALSE) %>%
+    hc_exporting(enabled = TRUE)
 
   if (path) { # add mean point to path
-
+    dfobj_path = data.frame()
     for (i in 1:length(cellValues)) {
       x <- mean(as.numeric(dfobj_new$x[cellValues[[i]]]))
       y <- mean(as.numeric(dfobj_new$y[cellValues[[i]]]))
-      dfobj_new <- rbind(dfobj_new, c(x, y, "meanPoint"))
-      colV <- c(colV, "#000000")
+      dfobj_path <- rbind(dfobj_path, c(x, y))
     }
-    newIdx <- (nrow(dfobj) + 1):nrow(dfobj_new)
-    cellValues <- c(unname(unlist(cellValues)), newIdx)
+    colnames(dfobj_path) = c('x','y')
 
-    dfobj_new$x <- round(as.numeric(dfobj_new$x), 4)
-    dfobj_new$y <- round(as.numeric(dfobj_new$y), 4)
+    hc <- hc %>%
+      hc_add_series(dfobj_path, 'line', hcaes(x = x, y = y), showInLegend = FALSE) %>%
+      hc_plotOptions(
+        line = list(
+          marker = FALSE,
+          dashStyle = 'ShortDash',
+          lineColor = '#5f27cd',
+          lineWidth = 2
+        ))
 
-    for (i in 1:(length(newIdx) - 1)) { # add curve
-      newCurve <- paste(
-        " + geom_curve( aes(x = ", "x[newIdx[", i,
-        "]], y = y[newIdx[", i, "]], xend = x[newIdx[", i + 1,
-        "]], yend = y[newIdx[", i + 1, ']]), size = 0.5, linetype = "longdash",',
-        "curvature = 0.1, colour = '#000000', ", 'arrow = arrow(length = unit(0.1,"inches")))'
+    #newIdx <- (nrow(dfobj) + 1):nrow(dfobj_new)
+    #cellValues <- c(unname(unlist(cellValues)), newIdx)
+
+    #dfobj_new$x <- round(as.numeric(dfobj_new$x), 4)
+    #dfobj_new$y <- round(as.numeric(dfobj_new$y), 4)
+
+    #for (i in 1:(length(newIdx) - 1)) { # add curve
+      #newCurve <- paste(
+        #" + geom_curve( aes(x = ", "x[newIdx[", i,
+        #"]], y = y[newIdx[", i, "]], xend = x[newIdx[", i + 1,
+        #"]], yend = y[newIdx[", i + 1, ']]), size = 0.5, linetype = "longdash",',
+        #"curvature = 0.1, colour = '#000000', ", 'arrow = arrow(length = unit(0.1,"inches")))'
         # "curvature = 0.1, colour = colV[newIdx[", i, ']], arrow = arrow(length = unit(0.1,"inches")))'
-      )
-      graphString <- paste(graphString, newCurve, sep = "")
-    }
+      #)
+      #graphString <- paste(graphString, newCurve, sep = "")
+    #}
   }
 
-  eval(parse(text = graphString))
-  return(ggobj2)
+  #  eval(parse(text = graphString))
+  return(hc)
 }
